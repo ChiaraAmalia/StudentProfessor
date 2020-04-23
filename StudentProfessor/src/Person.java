2cb425c9d0f9aef63b4297a3df8e43622ecbb930
Chiara Amalia Caporusso <s1087171@studenti.univpm.it>
2020-04-23 18:44:47 +0200

    modifica 3

--- StudentProfessor/src/Person.java
+++ StudentProfessor/src/Person.java
@@ -4,6 +4,7 @@ public class Person {
    
    // Constructor
    //ciao come va
+   //ciao tutto bene
    public Person(String name, String address) {
       this.name = name;
       this.address = address;
